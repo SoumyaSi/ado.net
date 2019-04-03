e7b0586d5654c4a12b96989e94f75bdb5732602a
soumya sinha <soumya.sinha@gridinfocom.com>
2019-04-03 11:25:05 +0530

    first commit

---
+++ crudoperation/crud.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="crud.aspx.cs" Inherits="crudoperation.crud" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox runat="server" ID="Nametxt" ></asp:TextBox>
+            <%--<asp:Button Text="Submit" runat="server" OnClick="Unnamed_Click"/>--%>
+            <asp:TextBox runat="server" ID="IDtxt" ></asp:TextBox>
+            <asp:Button Text="Update" runat="server" OnClick="Unnamed_Click1"/>
+            <asp:Button Text="delete" runat="server" OnClick="Unnamed_Click"/>
+            
+            
+        </div>
+        <asp:GridView ID="GridView1" runat="server" BackColor="#DEBA84" AutoGenerateColumns="false" BorderColor="#DEBA84" BorderStyle="None" BorderWidth="1px" CellPadding="3" CellSpacing="2">
+            <Columns>
+                 <asp:BoundField HeaderText="ID" DataField="ID" />
+                <asp:BoundField HeaderText="Name" DataField="studentname" />
+            </Columns>
+            <FooterStyle BackColor="#F7DFB5" ForeColor="#8C4510" />
+            <HeaderStyle BackColor="#A55129" Font-Bold="True" ForeColor="White" />
+            <PagerStyle ForeColor="#8C4510" HorizontalAlign="Center" />
+            <RowStyle BackColor="#FFF7E7" ForeColor="#8C4510" />
+            <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#FFF1D4" />
+            <SortedAscendingHeaderStyle BackColor="#B95C30" />
+            <SortedDescendingCellStyle BackColor="#F1E5CE" />
+            <SortedDescendingHeaderStyle BackColor="#93451F" />
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ crudoperation/crud.aspx.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace crudoperation
+{
+    public partial class crud : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("data source=LAPTOP-1RTEK74K;database=GIC;integrated security=true;");
+            SqlCommand com = new SqlCommand("select * from student", con);
+            con.Open();
+            SqlDataReader rdr = com.ExecuteReader();
+            GridView1.DataSource = rdr;
+            GridView1.DataBind();
+            con.Close();
+
+
+
+
+            //SqlConnection con = new SqlConnection("data source=LAPTOP-1RTEK74K;database=GIC;integrated security=true;");
+            //SqlCommand com = new SqlCommand("update student set studentname='riya',where ID=1,con");
+            //con.Open();
+            //int i=com.ExecuteNonQuery();
+            //GridView1.DataSource = i;
+            //GridView1.DataBind();
+            //con.Close();
+            //Response.Write("record is updated");
+
+            // SqlConnection con = new SqlConnection("data source=LAPTOP-1RTEK74K;database=GIC;integrated security=true;");
+            //SqlCommand com = new SqlCommand("delete from student where ID=1",con);
+            //con.Open();
+            //int i=com.ExecuteNonQuery();
+            //GridView1.DataSource = i;
+            //GridView1.DataBind();
+            //con.Close();
+            //Response.Write("record is deleted");
+
+
+        }
+
+        //protected void Unnamed_Click(object sender, EventArgs e)
+        //{
+        //    SqlConnection con = new SqlConnection("data source=LAPTOP-1RTEK74K;database=GIC;integrated security=true;");
+        //    SqlCommand com = new SqlCommand("insert into student(studentname) values(@name)", con);
+
+        //    con.Open();
+        //    //SqlCommand com = new SqlCommand("insert into student(ID,studentname) values(1, 'Ajay')",con);
+        //    //com.Parameters.AddWithValue("ID", ID);
+        //    com.Parameters.AddWithValue("@name", Nametxt.Text);
+
+
+        //    int i = com.ExecuteNonQuery();
+        //    //GridView1.DataSource = i;
+        //    //GridView1.DataBind();
+        //    con.Close();
+        //    Response.Write("record is inserted");
+
+
+        
+
+        protected void Unnamed_Click1(object sender, EventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection("data source=LAPTOP-1RTEK74K;database=GIC;integrated security=true;");
+            SqlCommand com = new SqlCommand("update student set studentname=@name where ID=@ID", con);
+
+            con.Open();
+            //SqlCommand com = new SqlCommand("insert into student(ID,studentname) values(1, 'Ajay')",con);
+            //com.Parameters.AddWithValue("ID", ID);
+            com.Parameters.AddWithValue("@ID", IDtxt.Text);
+            com.Parameters.AddWithValue("@name", Nametxt.Text);
+
+
+            int i = com.ExecuteNonQuery();
+            //GridView1.DataSource = i;
+            //GridView1.DataBind();
+            con.Close();
+            Response.Write("record is updated");
+
+
+        }
+
+        protected void Unnamed_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("data source=LAPTOP-1RTEK74K;database=GIC;integrated security=true;");
+            SqlCommand com = new SqlCommand("delete student where ID=@ID", con);
+
+            con.Open();
+            //SqlCommand com = new SqlCommand("insert into student(ID,studentname) values(1, 'Ajay')",con);
+            //com.Parameters.AddWithValue("ID", ID);
+            com.Parameters.AddWithValue("@ID", IDtxt.Text);
+           // com.Parameters.AddWithValue("@name", Nametxt.Text);
+
+
+            int i = com.ExecuteNonQuery();
+            //GridView1.DataSource = i;
+            //GridView1.DataBind();
+            con.Close();
+            Response.Write("record is deleted");
+
+
+        }
+    }
+}
